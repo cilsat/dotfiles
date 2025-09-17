a64ac4948ead6954ee8e55554bdadef11a671c95
Cil Satriawan <cilsat@Cils-MacBook-Air.local>
2025-09-17 23:13:09 +0700

    initial commit

--- arch-install-base.sh
+++
@@ -1,5 +0,0 @@
-pacman -S zsh git tmux neovim htop cowfortune
-
-pacman -S asp python python-pipenv networkmanager
-
-pacman -S lightdm xorg-server xf86-video-intel
--- cpu_now.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-# Current average CPU load
-#mpstat 2 1 | awk '$13 ~ /[0-9.]+/ {printf(" %.1f%", 100-$13)}'
-# Current average CPU temperature
-awk '{printf(" %.1f℃",$1*10^-3)}' /sys/devices/platform/thinkpad_hwmon/hwmon/*/temp1_input
-# Current average CPU clock
-#sort /sys/devices/system/cpu/cpu*/cpufreq/scaling_cur_freq | tail -n 1 |\
-#  awk '{printf(" %.1fGHz",$1*10^-6)}'
